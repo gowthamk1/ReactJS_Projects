7c77ae873f123059733529dfd1b16f8a8d2a3da7
Gowtham K <gowthamkpvt@gmail.com>
2025-03-15 20:49:37 +0530

    Added theme switcher feature

---
+++ 09themeSwitcher/tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+  darkMode: "class", // Enables dark mode using the 'class' method
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Scan these files for Tailwind classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
